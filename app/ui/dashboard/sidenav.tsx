d9a0eba8c197f141ca7254a8385aded1fbd7e1e2
Yohana Griselda <yohanagayatri@gmail.com>
2026-05-28 12:03:22 +0700

    update

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -23,7 +23,7 @@ export default function SideNav() {
         <form
           action={async () => {
             'use server';
-            await signOut({ redirectTo: '/' });
+            await signOut({ redirectTo: '/login' });
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-red-100 hover:text-red-600 md:flex-none md:justify-start md:px-3">
